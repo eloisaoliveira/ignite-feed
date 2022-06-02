b7eb762efd5a859562996b30b8a6a70f13e63358
EloisaOliveira <eloisa.oliveirasantos@gmail.com>
2022-06-02 20:42:47 -0300

    componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 import { Post } from './Post'
+
+import styles from './App.module.css'
+
 import './global.css'
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post />
-      <Post />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post />
+          <Post />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1618388810903-840bb0d15ea5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+      
+      <div className={styles.profile}>
+        <strong>Eloisa de Oliveira</strong>
+        <span>Web Developer</span>
+      </div>
+      <footer>
+        <a href="#">Editar seu perfil</a>
+      </footer>
+    </aside>
+  )
+}
