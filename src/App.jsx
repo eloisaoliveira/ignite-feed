3a054bfa4f028478de814241df01c895e748fc9e
EloisaOliveira <eloisa.oliveirasantos@gmail.com>
2022-06-02 19:12:11 -0300

    componente: Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Header } from './components/Header'
 import { Post } from './Post'
 import './global.css'
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post />
       <Post />
     </div>
