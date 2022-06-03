bcd30caffe3e3c44d5412a68ca3c1088d854824b
EloisaOliveira <eloisa.oliveirasantos@gmail.com>
2022-06-03 15:31:25 -0300

    finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -9,11 +11,15 @@ export function Sidebar() {
       />
       
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/EloisaOliveira.png" />
         <strong>Eloisa de Oliveira</strong>
         <span>Web Developer</span>
       </div>
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   )
