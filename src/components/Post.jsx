7229080ba3a5d26204ae0a33c31fd9dc9b215e75
EloisaOliveira <eloisa.oliveirasantos@gmail.com>
2022-06-07 16:43:57 -0300

    estilizações do Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -6,20 +6,25 @@ export function Post() {
       <header>
         <div className={styles.author}>
           <img className={styles.avatar} src="https://github.com/EloisaOliveira.png" />
-          <div className={styles.autjorInfo}>
+          <div className={styles.authorInfo}>
             <strong>Eloisa de Oliveira</strong>
             <span>Web Developer</span>
           </div>
         </div>
 
         <time title="11 de Maio às 08:13" dateTime="2022-05-11 08:13:30">Publicado há 1h</time>
+      </header>
+      
         <div className={styles.content}>
           <p>Fala galeraa 👋</p>
           <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-          <p><a href="">👉 jane.design/doctorcare</a></p>
-          <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+          <p><a href="">👉{' '} jane.design/doctorcare</a></p>
+          <p>
+            <a href="">#novoprojeto</a>{' '}
+            <a href="">#nlw</a>{' '}
+            <a href="">#rocketseat</a>
+          </p>
         </div>
-      </header>
     </article>
   )
 }
